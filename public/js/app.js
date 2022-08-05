aa2b2929aa18b2a23c065fb3cb885a23ed830243
lcylcy66666 <lcylcy66666@gmail.com>
2022-08-05 16:29:22 +0800

    Setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
+console.log('hi')
 weatherForm.addEventListener('submit', (e)=>{
     e.preventDefault()  
     
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit', (e)=>{
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
     
-    fetch('http://localhost:3000/weather?address='+ location).then((response)=>{
+    fetch('/weather?address='+ location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             messageOne.textContent = data.error
